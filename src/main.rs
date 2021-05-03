b9a6804b28d8fd3bb9298878dddb2eaa5d2b1d00
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-05-03 22:33:58 +0500

    add main func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+/*!
+An example Rust web application.
+
+The project is split into two main parts:
+
+- `app`: the rocket web application where the app is configured and hosted
+- `domain`: the business domain where the app logic is defined
+
+Most of the `domain` module is `pub(restricted)`, so these docs only show the items that can be consumed by the application.
+Refer to the source for a fuller picture of what's in there.
+*/
+
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[macro_use]
+extern crate log;
+
+pub mod app;
+pub mod domain;
+pub mod logger;
+
 fn main() {
-    println!("Hello, world!");
+    logger::init();
+    app:start();
 }
